1c0e251e3a38ec61460c5cac453fdbe51470d933
Aleksey Gorbunov <aleksey.g@alyce.com>
2022-12-05 14:36:12 +0300

    QAI-10351: Redirect to / if route isn't found

--- src/modules/DashboardModule/components/DashboardRoi/components/SfdcRoiDashboard/SfdcRoiReporting/SfdcRoiReporting.tsx
+++ src/modules/DashboardModule/components/DashboardRoi/components/SfdcRoiDashboard/SfdcRoiReporting/SfdcRoiReporting.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Grid } from '@mui/material';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import SfdcPipelineSummary from '../SfdcPipelineSummary/SfdcPipelineSummary';
-import { ROI_ROUTES } from '../../../routePaths';
+import { ROI_ROOT, ROI_ROUTES } from '../../../routePaths';
 
 import SfdcRoiInfluencedARR from './SfdcRoiInfluencedArr/SfdcRoiInfluencedArr';
 import SfdcInfluencedAccountTable from './SfdcInfluencedAccountTable/SfdcInfluencedAccountTable';
@@ -21,6 +21,7 @@ const SfdcRoiReporting = (): JSX.Element => (
       <Switch>
         <Route exact path={ROI_ROUTES.REPORTING} component={SfdcInfluencedAccountTable} />
         <Route exact path={`${ROI_ROUTES.REPORTING}/:accountId/accepted-gifts`} component={GiftsByAccountTable} />
+        <Redirect from="*" to={`/${ROI_ROOT}`} />
       </Switch>
     </Grid>
   </Grid>
